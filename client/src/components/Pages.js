436708b66a4622624197df2c1b043346e3ca75dc
Will <wmaccarty@gmail.com>
2020-09-02 18:27:20 -0400

    added impossible to find login page to website for fun

--- client/src/components/Pages.js
+++ client/src/components/Pages.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
+import Home from './Home';
+import LoginPage from './LoginPage';
+import { AuthRoute } from './Routes';
+
 const Pages = () => {
   return (
     <>
       <Switch>
+        <AuthRoute path='/login' exact component={LoginPage} />
+        <Route path='/' exact component={Home} />
       </Switch>
     </>
   )
