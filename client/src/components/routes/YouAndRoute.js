deee7dbecc1b2f106b4e1866d739423c61717950
Will <wmaccarty@gmail.com>
2020-09-06 16:34:46 -0400

    built the skeleton of YouAndRoute without functioning links

--- client/src/components/routes/YouAndRoute.js
+++ client/src/components/routes/YouAndRoute.js
@@ -1,10 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Box, List, ListItem, ListItemText, Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { Rating } from '@material-ui/lab';
+
+const useStyles = makeStyles({
+  headerText: {
+    fontSize: '22px',
+    fontWeight: '500',
+    borderBottom: '7px solid rgb(51, 103, 153)'
+  },
+})
 
 const YouAndRoute = () => {
+  const classes = useStyles();
+  const currentUser = useSelector(state => state.auth);
+  const currentRoute = useSelector(state => state.routes);
+  const [toDo, setToDo] = useState(false)
+
+  const toDoToggle = (e) => {
+    e.preventDefault();
+    setToDo(!toDo);
+  }
 
   return (
-    <>
-    </>
+    <Box>
+      <Typography variant='h2' className={classes.headerText} >{"You & This Route"}</Typography>
+      <List dense>
+        <ListItem>
+          <ListItemText>Your To-Do List:</ListItemText>
+          <Link onClick={toDoToggle} >{ !toDo ? 'Add To-Do - ' : 'On The List! - ' }</Link>
+          <Link to={`users/${currentUser.id}`} >View List</Link>
+        </ListItem>
+        <ListItem>
+          <ListItemText>Your Star Rating:</ListItemText>
+          <Rating max={4} />
+        </ListItem>
+        <ListItem>
+          <ListItemText>Your Difficulty Rating:</ListItemText>
+          <Typography>-none-</Typography>
+          <Link>{'  Change'}</Link>
+        </ListItem>
+        <ListItem>
+          <ListItemText>Your Ticks:</ListItemText>
+          <Link>Add New Tick</Link>
+
+        </ListItem>
+      </List>
+    </Box>
   );
 }
 
